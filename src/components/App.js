a2edca86bde1cd3d6b2da173a9a66f379b4ae267
MMcClure11 <mcclme02@luther.edu>
2020-11-03 13:28:06 -0600

    adds filter pigs button, renders filtered pigs to dom

--- src/components/App.js
+++ src/components/App.js
@@ -10,7 +10,7 @@ class App extends Component {
     return (
       <div className="App">
         <Nav />
-        <Filter hogs={hogs}/>
+        {/* <Filter hogs={hogs}/> */}
         <PigsList hogs={hogs} />
       </div>
     );
--- src/components/PigsList.js
+++ src/components/PigsList.js
@@ -3,19 +3,24 @@ import PigTile from './PigTile'
 
 class PigsList extends Component {
 
-  // constructor() {
-  //   super()
-  //   this.state = {
-  //     name: '',
-  //     specialty: '',
-  //     greased: '',
-  //     weight: '',
-  //     'height medal achieved': '',
-  //   }
-  // }
+  constructor(props) {
+    super(props)
+    this.state = {
+      pigs: props.hogs,
+      filteredPigs: props.hogs,
+    }
+  }
+
+  filterPigs = () => {
+    let filteredPigs = this.state.pigs.filter(pig => pig.greased === true)
+    console.log(filteredPigs)
+    this.setState({
+      filteredPigs: filteredPigs
+    })
+  }
 
   generatePigTiles = () => {
-    return this.props.hogs.map((data, i) => {
+    return this.state.filteredPigs.map((data, i) => {
       return <PigTile key={i} 
         name={data['name']} 
         specialty={data['specialty']} 
@@ -27,8 +32,11 @@ class PigsList extends Component {
 
   render() {
     return (
-      <div className='ui grid container ui cards'>
-        {this.generatePigTiles()}
+      <div>
+        <button onClick={this.filterPigs}>Filter Pigs</button>
+        <div className='ui grid container ui cards' >
+          {this.generatePigTiles()}
+        </div>
       </div>
     );
   }
